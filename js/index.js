6b33edb4c5abd9cbeb6f4f30a6b8297382712c1f
v-efimov-2807 <56639371+v-efimov-2807@users.noreply.github.com>
2020-05-17 18:37:47 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 "use strict";
 
-//Не вышло
-//import  Student from "./Student.js"
-//import months from "./months.js"
-
 let months=[
     'Января',
     'Февраля',
@@ -19,6 +15,19 @@ let months=[
     'Декабря',
 ];
 
+function createSimpleNode(tag, className, parentNode) {
+    const node = document.createElement(tag);
+    node.className = className;
+    return parentNode.appendChild(node);
+};
+
+function createSimpleNodeWithData(tag, className, parentNode, data) {
+    const node = document.createElement(tag);
+    node.className = className;
+    node.innerText = data;
+    return parentNode.appendChild(node);
+};
+
 class Student {
     constructor(params) {
         this.fullName = params.fullName;
@@ -33,9 +42,18 @@ class Student {
     };
 
     get age() {
-        let date = new Date();
-        let year = date.getFullYear();
-        return Math.floor( year - this.birthDate.getFullYear() ) + " лет";
+        const exceptions = [11, 12, 13, 14, 111, 112, 113, 114];
+        const agePerson = parseInt((Date.now() - this.birthDate) / 3600 / 24 / 365.25 / 1000);
+        
+        if ( exceptions.indexOf(agePerson) !== -1 ) {
+            return agePerson + " лет";
+        } else if ( (agePerson % 10) === 1 ) {
+            return agePerson + " год";
+        } else if ( ((agePerson % 10) === 2) || ((agePerson % 10) === 2) || ((agePerson % 10) === 2) ) {
+            return agePerson + " года";
+        }
+        
+        return agePerson + " лет";
     };
 
     get birthDateStr() {
@@ -78,49 +96,52 @@ class Student {
         const persons = document.getElementById("persons");
         const x = currentTarget.offsetTop;
         const y = currentTarget.offsetLeft;
-    
+
         const page = document.getElementById("page");
-            page.style.backgroundColor = "#808080"
-    
+            page.style.backgroundColor = "#808080";
+      
         const personPopup = createSimpleNode("div","personPopup", persons);
             personPopup.style.left = y + "px";
             personPopup.style.top = x + "px ";
-
-        const personPopup__info = createSimpleNode("div","personPopup__info", personPopup);
-            createSimpleNodeWithData("span", "personPopup__name ", personPopup__info, this.fullName);
-
-        const personPopup__wrap = createSimpleNode("div", "personPopup__wrap", personPopup__info);
-            createSimpleNodeWithData("div", "personPopup__caption", personPopup__wrap, "День рождения");
-            createSimpleNodeWithData("div", "personPopup__birthDate", personPopup__wrap, this.birthDateStr + ", " + this.age);
-            createSimpleNodeWithData("div", "personPopup__caption", personPopup__wrap, "Учится");
-            createSimpleNodeWithData("div", "personPopup__education", personPopup__wrap, this.education);
-
-        const personPopup__avatar = createSimpleNode("img","personPopup__avatar", personPopup);
-            personPopup__avatar.setAttribute("src", this.photoUrl);
-
-        const personPopup__exit = createSimpleNodeWithData("div", "personPopup__exit", personPopup, "X");
-            personPopup__exit.style.left = 550 + "px";
-            personPopup__exit.style.top = 5 + "px ";
-            personPopup__exit.addEventListener("click", (e) => {
-                personPopup.style.display = "none";
-                page.style.backgroundColor = "#ffffff"
-            });
         
-        //Как эти функции сделать глобальными на уровне класса?
-        function createSimpleNode(tag, className, parentNode) {
-            const node = document.createElement(tag);
-            node.className = className;
-            return parentNode.appendChild(node);
-        };
+        const personPopupInfo = createSimpleNode("div","personPopup__info", personPopup);
+            createSimpleNodeWithData("span", "personPopup__name ", personPopupInfo, this.fullName);
 
-        function createSimpleNodeWithData(tag, className, parentNode, data) {
-            const node = document.createElement(tag);
-            node.className = className;
-            node.innerText = data;
-            return parentNode.appendChild(node);
-        };
+        const personPopupWrap = createSimpleNode("div", "personPopup__wrap", personPopupInfo);
+            createSimpleNodeWithData("div", "personPopup__caption", personPopupWrap, "День рождения");
+            createSimpleNodeWithData("div", "personPopup__birthDate", personPopupWrap, this.birthDateStr + ", " + this.age);
+            createSimpleNodeWithData("div", "personPopup__caption", personPopupWrap, "Учится");
+            createSimpleNodeWithData("div", "personPopup__education", personPopupWrap, this.education);
+
+        const personPopupAvatar = createSimpleNode("img","personPopup__avatar", personPopup);
+            personPopupAvatar.setAttribute("src", this.photoUrl);
+
+        const personPopupExit = createSimpleNodeWithData("div", "personPopup__exit", personPopup, "X");
+            personPopupExit.style.left = 550 + "px";
+            personPopupExit.style.top = 5 + "px ";
+            personPopupExit.addEventListener("click", () => {
+                personPopup.style.display = "none";
+                page.style.backgroundColor = "#fff";
+            });
+
+        document.addEventListener('mousedown', function(e){
+            if(e.target.closest('.personPopup') === null){
+                personPopup.style.display = 'none';
+                page.style.backgroundColor = "#fff";
+            }
+        });
+
+        /* 
+        Вот это не работает. Почему? (popupIsActive - булева переменная, она была инициализирована, просто уже убрал)
+        этот if работает параллельно с первым кликом по котору мы попадаем в openCard, хотя по логике не должен
+        if (popupIsActive) {
+            document.onclick = function(event) {
+                if ( event.target.className !== 'personPopup' ) {
+                    personPopup.style.display = 'none';
+                };
+            };
+        };*/
     };
-   
 };
 
 const studentArr = [
@@ -135,28 +156,28 @@ const studentArr = [
         fullName: 'Маша Сидорова',
         university: 'МГУ',
         course: 4,
-        birthDate: new Date(1960, 0, 1),
+        birthDate: new Date(2009, 0, 1),
         photoUrl: "img/person/ava02.jpg"
     },
     {
         fullName: 'Вася Иванов',
         university: 'УГАТУ',
         course: 2,
-        birthDate: new Date(2000, 0, 1),
+        birthDate: new Date(1999, 0, 1),
         photoUrl: "img/person/ava03.jpg"
     },
     {
         fullName: 'Вася Иванов',
         university: 'УГАТУ',
         course: 2,
-        birthDate: new Date(2000, 0, 1),
+        birthDate: new Date(1998, 0, 1),
         photoUrl: "img/person/ava04.jpg"
     },
     {
         fullName: 'Вася Иванов',
         university: 'НГУ',
         course: 2,
-        birthDate: new Date(1999, 11, 11),
+        birthDate: new Date(2000, 6, 11),
         photoUrl: "img/person/ava05.jpg"
     },
     {
